e85a71f772a9f405f0e823ee52aa627963a87e9d
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-08-29 16:53:13 +0100

    Test pass: Returns a new string with a hashtag

--- lib/hashtag.rb
+++ lib/hashtag.rb
@@ -1,2 +1,13 @@
-class Hashtagger
+class String
+  def hashtagger
+    self.split.map do |e|
+      "##{e}"
+    end.join(" ")
+  end
 end
+
+
+# str = 'word'
+# break the words included in a string
+# add a hastag in the beginning of each one of them
+#join the string into a new string
--- spec/hashtag_spec.rb
+++ spec/hashtag_spec.rb
@@ -1,6 +1,8 @@
 require "hashtag"
 
-describe Hashtagger do
+describe "hashtagger method" do
   it "should hashtag all words in a given string and return a string" do
+    str = "word"
+    expect(str.hashtagger).to eq("#word")
   end
 end
