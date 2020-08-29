c51dd7bda67a2729e58147ed6d49a28ecfd97c61
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-08-29 16:54:35 +0100

    Test pass: it adds hashtags in all words in a given string

--- spec/hashtag_spec.rb
+++ spec/hashtag_spec.rb
@@ -1,8 +1,13 @@
 require "hashtag"
 
 describe "hashtagger method" do
-  it "should hashtag all words in a given string and return a string" do
+  it "should hashtag a words in a given string and return a new string" do
     str = "word"
     expect(str.hashtagger).to eq("#word")
+  end
+
+  it "should hashtag all words in a given string and return a new string" do
+    str = " random word"
+    expect(str.hashtagger).to eq("#random #word")
   end
 end
